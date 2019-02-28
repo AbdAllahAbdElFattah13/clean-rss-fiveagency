84f0c0cd99815e91647376339ffcf3938bc541a7
AbdAllah <AbdAllah.abdelfattah@thed.io>
2019-02-28 18:32:20 +0200

    Added live data instances in HomeViewModel as well methods to handle the interaction from the user.

--- presentation/src/main/java/com/github/abdallahabdelfattah13/presentation/viewmodel/home/HomeViewModel.kt
+++ presentation/src/main/java/com/github/abdallahabdelfattah13/presentation/viewmodel/home/HomeViewModel.kt
@@ -1,12 +1,16 @@
 package com.github.abdallahabdelfattah13.presentation.viewmodel.home
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
+import com.github.abdallahabdelfattah13.domain.model.Feed
 import com.github.abdallahabdelfattah13.domain.usecase.feed.AddNewFeedUseCase
 import com.github.abdallahabdelfattah13.domain.usecase.feed.DeleteFeedUseCase
 import com.github.abdallahabdelfattah13.domain.usecase.feed.GetAllFeedsUseCase
 import com.github.abdallahabdelfattah13.presentation.viewmodel.BaseViewModel
 import io.reactivex.Scheduler
+import io.reactivex.functions.Action
+import io.reactivex.functions.Consumer
 
 
 /**
@@ -15,14 +19,60 @@ import io.reactivex.Scheduler
  * Cairo, Egypt.
  */
 class HomeViewModel(
-    private val subscribeOn: Scheduler,
-    private val observeOn: Scheduler,
+    subscribeOn: Scheduler,
+    observeOn: Scheduler,
     private val addNewFeedsUseCase: AddNewFeedUseCase,
     private val getAllFeedsUseCase: GetAllFeedsUseCase,
     private val deleteFeedUseCase: DeleteFeedUseCase
 ) : BaseViewModel(subscribeOn, observeOn) {
 
-//TODO(need to lay down the livedata instances the view will be observing from each given usecase)
+    val addFeedSucessLiveData: MutableLiveData<Boolean> = MutableLiveData()
+    val deleteFeedSucessLiveData = MutableLiveData<Boolean>()
+    val feedsLiveData = MutableLiveData<List<Feed>>()
+
+    fun addFeed(url: String) {
+        executeCompletable(
+            Consumer {
+            },
+            Action {
+                addFeedSucessLiveData.postValue(true)
+            },
+            Consumer {
+                addFeedSucessLiveData.postValue(false)
+            },
+            addNewFeedsUseCase.run(url)
+        )
+    }
+
+    fun deleteFeed(feedId: Int) {
+        executeCompletable(
+            Consumer {
+            },
+            Action {
+                deleteFeedSucessLiveData.postValue(true)
+            },
+            Consumer {
+                deleteFeedSucessLiveData.postValue(false)
+            },
+            deleteFeedUseCase.run(feedId)
+        )
+    }
+
+    fun getFeeds() {
+        executeFlowable(
+            Consumer {
+
+            },
+            Consumer {
+                feedsLiveData.postValue(it)
+            },
+            Consumer {
+                feedsLiveData.postValue(emptyList())
+            },
+            getAllFeedsUseCase.run()
+        )
+    }
+
 }
 
 class HomeViewModelFactory(
