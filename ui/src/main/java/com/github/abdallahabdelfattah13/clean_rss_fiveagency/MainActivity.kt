18cc9dc15f74b0645a7ba90fc2b726947b2b7f35
AbdAllah <AbdAllah.abdelfattah@thed.io>
2019-03-01 14:08:42 +0200

    Some missing dependencies to the data/ui modules; Added file to .gitignore file; First buildable commit.

--- ui/src/main/java/com/github/abdallahabdelfattah13/clean_rss_fiveagency/MainActivity.kt
+++ ui/src/main/java/com/github/abdallahabdelfattah13/clean_rss_fiveagency/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.github.abdallahabdelfattah13.clean_rss_fiveagency
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
