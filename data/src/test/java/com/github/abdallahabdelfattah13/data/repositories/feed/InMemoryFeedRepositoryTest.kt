705c6b32286b15eae4be6dcb8bfa5a957073ea20
AbdAllah <AbdAllah.abdelfattah@thed.io>
2019-03-24 22:39:20 +0200

    Added InMemoryFeedRepositoryTest file which contains InMemoryFeedRepository test cases.
    Added the main 4 unit tests for createNewFeed method.

---
+++ data/src/test/java/com/github/abdallahabdelfattah13/data/repositories/feed/InMemoryFeedRepositoryTest.kt
@@ -0,0 +1,72 @@
+package com.github.abdallahabdelfattah13.data.repositories.feed
+
+import com.github.abdallahabdelfattah13.domain.model.Feed
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Created by AbdAllah AbdElfattah on 24/03/2019,
+ * The D. GmbH,
+ * Cairo, Egypt.
+ */
+class InMemoryFeedRepositoryTest {
+
+    lateinit var feedRepository: InMemoryFeedRepository
+
+    @Before
+    fun setUp() {
+        feedRepository = InMemoryFeedRepository()
+    }
+
+    @Test
+    fun shouldCompleteCreateFeedSuccessfully() {
+        feedRepository.createNewFeed(getListOfFeedsUrl()[0])
+            .test()
+            .assertComplete()
+            .assertNoErrors()
+    }
+
+    @Test
+    fun shouldCreateFeedWithErrorInCaseOfEmptyFeedUrl() {
+        feedRepository.createNewFeed("")
+            .test()
+            .assertFailure(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    fun shouldCreateFeedWithErrorInCaseOfBlankFeedUrl() {
+        feedRepository.createNewFeed("                  ")
+            .test()
+            .assertFailure(IllegalArgumentException::class.java)
+    }
+
+    @Test
+    fun shouldCreateFeedWithErrorInCaseOfSameFeedUrl() {
+        val testFeedUrl = getListOfFeedsUrl()[0]
+        feedRepository.createNewFeed(testFeedUrl)
+
+        feedRepository.createNewFeed(testFeedUrl)
+            .test()
+            .assertFailure(IllegalArgumentException::class.java)
+    }
+
+    private fun mapStringToFeedObject(index: Int, s: String): Feed = Feed(
+        id = index,
+        url = s,
+        title = s,
+        description = "This is feed number: $index in your list",
+        thumbnail = ""
+    )
+
+    private fun getListOfFeedsUrl(): List<String> {
+        return listOf(
+            "http://feeds.bbci.co.uk/news/rss.xml",
+            "http://news.yahoo.com/rss/",
+            "http://feeds.bbci.co.uk/news/technology/rss.xml"
+        )
+    }
+
+    private fun getListOfFeedsObject(): List<Feed> {
+        return getListOfFeedsUrl().mapIndexed(::mapStringToFeedObject)
+    }
+}
