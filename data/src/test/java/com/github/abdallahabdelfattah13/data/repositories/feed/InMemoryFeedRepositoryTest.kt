dda58a93cc656a340f0ce33ebc89522aeebc0638
AbdAllah <AbdAllah.abdelfattah@thed.io>
2019-03-26 09:31:23 +0200

    Added test cases for getFeeds in InMemoryFeedRepositoryTest.

--- data/src/test/java/com/github/abdallahabdelfattah13/data/repositories/feed/InMemoryFeedRepositoryTest.kt
+++ data/src/test/java/com/github/abdallahabdelfattah13/data/repositories/feed/InMemoryFeedRepositoryTest.kt
@@ -18,6 +18,7 @@ class InMemoryFeedRepositoryTest {
         feedRepository = InMemoryFeedRepository()
     }
 
+    //region feedRepository.createNewFeed test cases
     @Test
     fun shouldCompleteCreateFeedSuccessfully() {
         feedRepository.createNewFeed(getListOfFeedsUrl()[0])
@@ -56,6 +57,55 @@ class InMemoryFeedRepositoryTest {
             .test()
             .assertError(IllegalArgumentException::class.java)
     }
+    //endregion
+
+    //region feedRepository.getFeeds test cases
+    @Test
+    fun getFeedsSuccessfully() {
+        val listOfFeeds = getListOfFeeds()
+        val testFeedsList = ArrayList<Feed>()
+
+        val testedFeedsObservable = feedRepository.getFeeds()
+
+        listOfFeeds.forEach {
+            feedRepository.createNewFeed(it.url)
+            testFeedsList.add(it)
+
+            testedFeedsObservable
+                .test()
+                .assertSubscribed()
+                .assertNoErrors()
+                .assertValue(testFeedsList)
+        }
+    }
+
+    @Test
+    fun getFeedsWillNotEmitInCaseOfBlankStrings() {
+        val testedFeedsObservable = feedRepository.getFeeds()
+
+        feedRepository.createNewFeed("    ")
+
+        testedFeedsObservable
+            .test()
+            .assertSubscribed()
+            .assertNoErrors()
+            .assertNever { emittedList -> emittedList.find { feed -> feed.url.isBlank() } != null }
+    }
+
+    @Test
+    fun getFeedsWillReturnLastStateUponSubscription() {
+        val testedFeedsObservable = feedRepository.getFeeds()
+        val feeds = getListOfFeeds()
+
+        feeds.forEach { feed -> feedRepository.createNewFeed(feed.url) }
+
+        testedFeedsObservable
+            .test()
+            .assertSubscribed()
+            .assertNoErrors()
+            .assertValue(feeds)
+    }
+    //endregion
 
     private fun mapStringToFeedObject(index: Int, s: String): Feed = Feed(
         id = index,
@@ -73,7 +123,7 @@ class InMemoryFeedRepositoryTest {
         )
     }
 
-    private fun getListOfFeedsObject(): List<Feed> {
+    private fun getListOfFeeds(): List<Feed> {
         return getListOfFeedsUrl().mapIndexed(::mapStringToFeedObject)
     }
 }
