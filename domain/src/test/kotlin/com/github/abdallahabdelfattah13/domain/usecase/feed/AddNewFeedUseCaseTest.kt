6c0e5decb68d2b049badb90907d893b474dafb46
Victor <victor.a.vicari@gmail.com>
2019-07-17 14:03:30 -0300

    I updated the unit test to MockK. To reduce boilerplate

--- domain/src/test/kotlin/com/github/abdallahabdelfattah13/domain/usecase/feed/AddNewFeedUseCaseTest.kt
+++ domain/src/test/kotlin/com/github/abdallahabdelfattah13/domain/usecase/feed/AddNewFeedUseCaseTest.kt
@@ -1,9 +1,10 @@
 package com.github.abdallahabdelfattah13.domain.usecase.feed
 
 import com.github.abdallahabdelfattah13.domain.repositories.feed.FeedRepository
-import com.github.abdallahabdelfattah13.domain.usecase.Mocks
-import com.nhaarman.mockito_kotlin.given
-import com.nhaarman.mockito_kotlin.mock
+import com.github.abdallahabdelfattah13.domain.usecase.Mocks.feedsUrl
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
 import io.reactivex.Completable
 import org.junit.Before
 import org.junit.Test
@@ -15,36 +16,21 @@ import org.junit.Test
  */
 class AddNewFeedUseCaseTest {
 
-    private lateinit var addFeedUseCase: AddNewFeedUseCase
-    private val BLANK_STRING_CASE = "      "
-    private val NONE_URL_STRING_CASE = "some random text that isn't a valid url and isn't empty!"
-
+    @MockK
+    internal lateinit var repo: FeedRepository
+    private val addFeedUseCase by lazy { AddNewFeedUseCase(repo) }
 
     @Before
     fun setUp() {
-        val repo: FeedRepository = mock()
-
-        Mocks.feedsUrl.forEach {
-            given(repo.createNewFeed(it)).willReturn(Completable.complete())
-        }
-
-        given(repo.createNewFeed("")).willReturn(Completable.error(IllegalArgumentException()))
-        given(repo.createNewFeed(BLANK_STRING_CASE)).willReturn(Completable.error(IllegalArgumentException()))
-        given(repo.createNewFeed(NONE_URL_STRING_CASE))
-            .willReturn(
-                Completable.error(
-                    IllegalArgumentException()
-                )
-            )
-
-        addFeedUseCase = AddNewFeedUseCase(repo)
+        MockKAnnotations.init(this)
     }
 
     @Test
     fun run_validUrlSuccess() {
-        val completableUnderTesting = addFeedUseCase.run(Mocks.feedsUrl[0])
-        completableUnderTesting
-            .test()
+        every { repo.createNewFeed(any()) } returns Completable.complete()
+
+        val result = addFeedUseCase.run(feedsUrl[0])
+        result.test()
             .assertComplete()
             .assertNoErrors()
     }
@@ -52,6 +38,8 @@ class AddNewFeedUseCaseTest {
 
     @Test
     fun run_invalidUrl_empty() {
+        every { repo.createNewFeed(any()) } returns Completable.complete()
+
         val completableUnderTesting = addFeedUseCase.run("")
         completableUnderTesting
             .test()
@@ -60,7 +48,9 @@ class AddNewFeedUseCaseTest {
 
     @Test
     fun run_invalidUrl_Blank() {
-        val completableUnderTesting = addFeedUseCase.run(BLANK_STRING_CASE)
+        every { repo.createNewFeed(any()) } returns Completable.complete()
+
+        val completableUnderTesting = addFeedUseCase.run("     ")
         completableUnderTesting
             .test()
             .assertFailure(IllegalArgumentException::class.java)
@@ -68,7 +58,9 @@ class AddNewFeedUseCaseTest {
 
     @Test
     fun run_invalidUrl_NoneEmptyString() {
-        val completableUnderTesting = addFeedUseCase.run(NONE_URL_STRING_CASE)
+        every { repo.createNewFeed(any()) } returns Completable.complete()
+
+        val completableUnderTesting = addFeedUseCase.run("loremIpsum")
         completableUnderTesting
             .test()
             .assertFailure(IllegalArgumentException::class.java)
